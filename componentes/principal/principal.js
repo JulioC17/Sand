fefc55c7e8787358937a63e9e10a6d4b86ac2365
JulioC17 <159253136+JulioC17@users.noreply.github.com>
2024-11-29 12:46:22 +0100

    componente principal terminado y agregado

--- componentes/principal/principal.js
+++ componentes/principal/principal.js
@@ -1,14 +1,62 @@
 import anime from "animejs";
 import './principal.css'
 
-const main = () => {
+const main = () => { //duncion que pinta los elementos de la pagina principal
         const name = localStorage.getItem("usuario")
         const work = localStorage.getItem("localTrabajo")
         
         document.querySelector("body").innerHTML = `
-            <h1>${name}</h1>
-           
+           <div id = "principal">
+                <nav id = "navbar">
+                    <button id = "historic">Histórico</button>
+                    <button id = "edit">Editar</button>
+                </nav>
+
+                <h1 id = "welcome">Bienvenido<span> ${name}</span></h1>
+            
+                <section id = "worked">
+                    <h3>Trabajado este mes:</h3>
+                    <h2>10 horas</h2>
+                </section>
+
+                <button id = "fichar">FICHAR</button>
+            
+           </div>
         `
+
+ anime({ // animaciones de Principal
+            targets: '#navbar',
+            translateY: 50,
+            opacity: [0, 1],
+            duration:1000,
+            easing: "easeOutElastic(0.5, 1)"
+});
+
+anime({ 
+        targets: '#welcome',
+        translateY: 50,
+        opacity: [0, 1],
+        duration:2000,
+        easing: "easeOutElastic(0.5, 1)"
+});
+
+anime({ 
+    targets: '#worked',
+    translateY: 50,
+    opacity: [0, 1],
+    duration:3000,
+    easing: "easeOutElastic(0.5, 1)"
+});
+
+anime({ 
+    targets: '#fichar',
+    translateY: -50,
+    opacity: [0, 1],
+    duration:2000,
+    easing: "easeOutElastic(0.5, 1)"
+});
+
+      
         
         
        
--- main.js
+++ main.js
@@ -8,7 +8,7 @@ import main from './componentes/principal/principal'
 const registeredUser = localStorage.getItem("usuario")
     if(registeredUser === null) {  //si no hay usuario registrado llama a login() que pinta el login y da una alerta si no escribe nada en los inputs o almacena los datos del ususario
         login()
-    }else if (registeredUser.length > 0){
+    }else if (registeredUser.length > 0){ //si el susuario esta registrado pero sin fichar carga la pagina principal de la app
         main()
     }
 
